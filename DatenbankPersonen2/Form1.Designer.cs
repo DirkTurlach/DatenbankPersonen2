c2d0896daf5064b430b17df006e3384cb06f9882
DirkTurlach <dirk.turlach@gmail.com>
2019-01-29 08:19:22 +0100

    Projektdateien hinzufügen.

---
+++ DatenbankPersonen2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DatenbankPersonen2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerbinden = new System.Windows.Forms.Button();
+            this.btnSchliessen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVerbinden
+            // 
+            this.btnVerbinden.Location = new System.Drawing.Point(70, 352);
+            this.btnVerbinden.Name = "btnVerbinden";
+            this.btnVerbinden.Size = new System.Drawing.Size(75, 23);
+            this.btnVerbinden.TabIndex = 0;
+            this.btnVerbinden.Text = "Verbinden";
+            this.btnVerbinden.UseVisualStyleBackColor = true;
+            this.btnVerbinden.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSchliessen
+            // 
+            this.btnSchliessen.Location = new System.Drawing.Point(234, 352);
+            this.btnSchliessen.Name = "btnSchliessen";
+            this.btnSchliessen.Size = new System.Drawing.Size(75, 23);
+            this.btnSchliessen.TabIndex = 1;
+            this.btnSchliessen.Text = "S&chließen";
+            this.btnSchliessen.UseVisualStyleBackColor = true;
+            this.btnSchliessen.Click += new System.EventHandler(this.btnSchliessen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSchliessen);
+            this.Controls.Add(this.btnVerbinden);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerbinden;
+        private System.Windows.Forms.Button btnSchliessen;
+    }
+}
+
---
+++ DatenbankPersonen2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DatenbankPersonen2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Adressen;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open  !");
+            
+            //Daten lesen
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            sql = "SELECT vorname, nachname FROM personen Where PersonenID < 5";
+            command = new SqlCommand(sql, cnn);
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + "\n";
+            }
+
+            MessageBox.Show(Output);
+
+            dataReader.Close();
+
+            // Daten schreiben
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            sql = "INSERT into personen (anrede, vorname, nachname, strasse, plz, ort, land) values('Herr' , 'SqlEingefuegtVor', 'SqlEingefuegtNach', 'Str' , 66666, 'oertchen', 'S')";
+
+            adapter.InsertCommand = new SqlCommand(sql, cnn);
+            adapter.InsertCommand.ExecuteNonQuery();
+
+
+            // Daten veraender
+            sql = "UPDATE personen SET vorname = 'SQLgeaender' WHERE PersonenId=6993";
+
+            adapter.UpdateCommand = new SqlCommand(sql, cnn);
+            adapter.UpdateCommand.ExecuteNonQuery();
+
+            ////Daten loeschen
+            //sql = "DELETE FROM personen WHERE PersonenID > 1000";
+
+            //adapter.DeleteCommand = new SqlCommand(sql, cnn);
+            //adapter.DeleteCommand.ExecuteNonQuery();
+
+
+            command.Dispose();
+            cnn.Close();
+        }
+
+        private void btnSchliessen_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
